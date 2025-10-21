04b282d2c5f1888e96613c6e9d3e783d258d13c4
Cătălin-Andrei Macovei <catalin_mcv@yahoo.com>
2025-10-21 15:09:09 +0300

    comit2

---
+++ aufgabe1/src/aufgabe2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] zahlen = {4, 8, 3, 10, 17};
+
+        System.out.println("Maximale Zahl: " + ZahlenAnalyse.maximaleZahl(zahlen));
+        System.out.println("Minimale Zahl: " + ZahlenAnalyse.minimaleZahl(zahlen));
+        System.out.println("Maximale Summe (n-1): " + ZahlenAnalyse.maximaleSumme(zahlen));
+        System.out.println("Minimale Summe (n-1): " + ZahlenAnalyse.minimaleSumme(zahlen));
+    }
+}
