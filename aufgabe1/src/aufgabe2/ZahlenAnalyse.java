4bd8b3f1da51a15d9a58c5dcda0477cc10a017c3
Cătălin-Andrei Macovei <catalin_mcv@yahoo.com>
2025-10-21 15:09:41 +0300

    comit4

---
+++ aufgabe1/src/aufgabe2/ZahlenAnalyse.java
@@ -0,0 +1,28 @@
+public class ZahlenAnalyse {
+
+    public static int maximaleZahl(int[] zahlen) {
+        int max = zahlen[0];
+        for (int z : zahlen)
+            if (z > max) max = z;
+        return max;
+    }
+
+    public static int minimaleZahl(int[] zahlen) {
+        int min = zahlen[0];
+        for (int z : zahlen)
+            if (z < min) min = z;
+        return min;
+    }
+
+    public static int maximaleSumme(int[] zahlen) {
+        int sum = 0, min = minimaleZahl(zahlen);
+        for (int z : zahlen) sum += z;
+        return sum - min;
+    }
+
+    public static int minimaleSumme(int[] zahlen) {
+        int sum = 0, max = maximaleZahl(zahlen);
+        for (int z : zahlen) sum += z;
+        return sum - max;
+    }
+}
